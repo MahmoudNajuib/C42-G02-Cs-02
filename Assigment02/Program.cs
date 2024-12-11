06e4e1af0bbd6efcd396ba51b428c124ac3f0155
SHJuba <elprince.dokdok@gmail.com>
2024-12-12 01:57:35 +0200

    Fisrt

--- Assigment02/Program.cs
+++ Assigment02/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, ssssssssWorld!");
+            Console.WriteLine("Hello, ssssssWorld!");
         }
     }
 }
